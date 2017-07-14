4d8fd07c48b553214689a80aee10f712454a9d71
Christopher Kolstad <christopher.kolstad@finn.no>
2017-07-14 16:09:39 +0200

    Set timezone to UTC

--- src/main/kotlin/com/chriswk/listicle/ListicleApplication.kt
+++ src/main/kotlin/com/chriswk/listicle/ListicleApplication.kt
@@ -2,10 +2,13 @@ package com.chriswk.listicle
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import java.util.*
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 open class ListicleApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault( TimeZone.getTimeZone( "UTC" ) );
     SpringApplication.run(ListicleApplication::class.java, *args)
 }
